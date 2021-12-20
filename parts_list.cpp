71287897d68ada5e982837f9349d8f03114cefea
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:49:12 +0300

    Update parts_list.cpp

--- parts_list.cpp
+++ parts_list.cpp
@@ -1,9 +1,9 @@
 #include "easy_list.h"
 
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
-    int count = mass.size();
-    if(count != 0){
-        for(int i = 0; i < count; i++){
+    int c = mass.size();
+    if(c != 0){
+        for(int i = 0; i < c; i++){
             if(mass[i] % 2 == 0){
                 mass2.push_back(mass[i]);
             }
